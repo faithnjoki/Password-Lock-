343b08cea5b499c5e314937d226c6520acf1d1be
faithnjoki <faithnjoki81@gmail.com>
2021-11-12 22:18:20 +0300

    add functions

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 name = input('What is your name? ')
 print(f'Hey {name}, Welcome to Pass Locker!')
 
-mode = input('What is the password? ')
-Todo = input('Woould you want to view existing passwords or create new password?(View or Add) ')
-if mode == 
+# add functions
+def view():
+    pass
+def add():
+    pass
+
+
+
+
+while True:
+    Mode = input('Woould you want to view existing passwords or create new password or quit?(View or Add or  quit) ').lower()
+    if Mode== 'quit':
+        break
+    elif Mode == 'view':
+        pass
+    elif Mode == 'add':
+        pass
+    else:
+        print('Invalid mode')
+        continue
