4f3a3831135f60fad1b40659161015597f573ea5
faithnjoki <faithnjoki81@gmail.com>
2021-11-12 22:12:28 +0300

    initialized repository

---
+++ main.py
@@ -0,0 +1,6 @@
+name = input('What is your name? ')
+print(f'Hey {name}, Welcome to Pass Locker!')
+
+mode = input('What is the password? ')
+Todo = input('Woould you want to view existing passwords or create new password?(View or Add) ')
+if mode == 
