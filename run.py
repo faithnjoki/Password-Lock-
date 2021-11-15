303b2c53ac706084744a2f93a59f5932858bbb60
faithnjoki <faithnjoki81@gmail.com>
2021-11-15 23:36:26 +0300

    lg and exit

--- run.py
+++ run.py
@@ -12,14 +12,59 @@ def main():
             print('Create username: ')
             createdusername = input()
 
+            print('Create Password: ')
+            createdpassword = input()
+
             print('Confirm Password: ')
-            cornfirmpassword = input()
+            confirmpassword = input()
 
+            while confirmpassword!=createdpassword:
+                print('Invalid Password')
+                print('Enter your Password; ')
+                createdpassword = input()
+                print('Confirm Password: ')
+                confirmpassword = input()
+            else:
+                print(f'Congatulations {createdusername}! account creation was successful')
+                print('\n')
+                print('Please, Proceede to login')
+                print('Username: ')
+                enteredusername = input()
+                print('Enter your password: ')
+                enteredpassword = input()
+            while enteredusername != createdusername or enteredpassword != createdpassword:
+                print('Invalid username or password')
+                print('Enter username: ')
+                enteredusername = input()
+                print('Entered password: ')
+                enteredpassword = input()
+            else:
+                print(f'Welcome {enteredusername} to your account')
+                print('\n')
 
-        
+        elif shortcode == 'lg':
+            print('Welcome')
+            print('Enter Your username: ')
+            defaultUsername = input()
+            print('Enter Your Password: ')
+            defaultpassword = input()
+            print('/n')
+            while defaultUsername != 'Faith' or defaultpassword !='leonbaby':
+                print("wrong user name or password . username:'Faith' password:'leonbaby'")
+                print('Enter username: ')
+                defaultUsername = input()
 
-
-     
+                print('Enter password: ')
+                defaultpassword = input()
+                print('\n')
+            else:
+                print('Login sucess')
+                print('\n')
+                print('\n')
+        elif shortcode == 'ex':
+            break
+        else:
+            print('Enter Valid code to continue')  
 
 
 if __name__ == "__main__":
